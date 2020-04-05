020b5a742c8c4fefec35be6674bdab8494f85878
Awe1alex <aleksejs.birjulins@gmail.com>
2020-04-05 16:43:41 +0300

    Working on login page

---
+++ client/src/components/AppForm.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="form-container" :class="{'form-container-sign-up': login}">
+    <form class="form">FORM</form>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    login: {
+      type: Boolean,
+      default: false
+    }
+  }
+};
+</script>
--- client/src/views/Login.vue
+++ client/src/views/Login.vue
@@ -1,11 +1,52 @@
 <template>
-  <div>Login</div>
+  <div class="login-page">
+    <div class="login-container">
+      <div class="row h-100">
+        <div class="col-1-of-2 h-100">
+          <div class="login-content">
+            <h2 class="heading-secondary">Don't have an account?</h2>
+            <span class="blue-line"></span>
+            <p
+              class="login-text"
+            >Lorem ipsum dolor sit, amet consectetur adipisicing elit. Illo, et, labore omnis consectetur esse ut quam eligendi nesciunt distinctio sequi recusandae cum nihil a tenetur!</p>
+            <button class="btn btn-primary" @click="toSignUp">Sign Up</button>
+          </div>
+        </div>
+        <div class="col-1-of-2 h-100">
+          <div class="login-content">
+            <h2 class="heading-secondary">Have an account?</h2>
+            <span class="blue-line"></span>
+            <p class="login-text">Lorem, ipsum dolor sit amet consectetur adipisicing elit.</p>
+            <button class="btn btn-primary" @click="toLogin">Login</button>
+          </div>
+        </div>
+      </div>
+      <app-form :login="login" />
+    </div>
+  </div>
 </template>
 
 <script>
 // @ is an alias to /src
+import AppForm from "@/components/AppForm.vue";
 
 export default {
-  name: "Home"
+  name: "Home",
+  components: {
+    AppForm
+  },
+  data() {
+    return {
+      login: false
+    };
+  },
+  methods: {
+    toLogin() {
+      this.login = true;
+    },
+    toSignUp() {
+      this.login = false;
+    }
+  }
 };
 </script>
