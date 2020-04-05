13af4c766d460cb857bb3aa01f22e1388d5c38a8
Awe1alex <aleksejs.birjulins@gmail.com>
2020-04-05 22:16:21 +0300

    ready login page

--- client/src/components/AppForm.vue
+++ client/src/components/AppForm.vue
@@ -1,16 +1,95 @@
 <template>
-  <div class="form-container" :class="{'form-container-sign-up': login}">
-    <form class="form">FORM</form>
-  </div>
+    <div class="container__form" :class="{ 'container__form--sign-up': login }">
+        <div class="logo__container">
+            <img src="../assets/logo.png" alt="Logo" class="logo__img" />
+        </div>
+        <transition name="fade">
+            <div v-if="login" class="forms--login">
+                <form class="form">
+                    <h2 class="heading--secondary">Login</h2>
+                    <span class="line line--blue"></span>
+                    <div class="form__group">
+                        <input
+                            type="email"
+                            id="email"
+                            class="form__input"
+                            placeholder="Email"
+                            required
+                        />
+                        <label for="email" class="form__label">Email</label>
+                        <i class="icon-email"></i>
+                    </div>
+                    <div class="form__group">
+                        <input
+                            type="password"
+                            id="password"
+                            class="form__input"
+                            placeholder="Password"
+                            required
+                        />
+                        <label for="password" class="form__label"
+                            >Password</label
+                        >
+                        <i class="icon-lock"></i>
+                    </div>
+                    <button class="btn btn--warning">Login</button>
+                </form>
+            </div>
+        </transition>
+        <transition name="fade">
+            <div v-if="!login" class="forms--sign-up">
+                <form class="form">
+                    <h2 class="heading--secondary">Sign Up</h2>
+                    <span class="line line--blue"></span>
+                    <div class="form__group">
+                        <input
+                            type="text"
+                            id="name"
+                            class="form__input"
+                            placeholder="Name"
+                            required
+                        />
+                        <label for="name" class="form__label">Name</label>
+                        <i class="icon-user"></i>
+                    </div>
+                    <div class="form__group">
+                        <input
+                            type="email"
+                            id="email"
+                            class="form__input"
+                            placeholder="Email"
+                            required
+                        />
+                        <label for="email" class="form__label">Email</label>
+                        <i class="icon-email"></i>
+                    </div>
+                    <div class="form__group">
+                        <input
+                            type="password"
+                            id="password"
+                            class="form__input"
+                            placeholder="Password"
+                            required
+                        />
+                        <label for="password" class="form__label"
+                            >Password</label
+                        >
+                        <i class="icon-lock"></i>
+                    </div>
+                    <button class="btn btn--warning">Sign Up</button>
+                </form>
+            </div>
+        </transition>
+    </div>
 </template>
 
 <script>
 export default {
-  props: {
-    login: {
-      type: Boolean,
-      default: false
-    }
-  }
-};
+    props: {
+        login: {
+            type: Boolean,
+            default: false,
+        },
+    },
+}
 </script>
--- client/src/views/Login.vue
+++ client/src/views/Login.vue
@@ -1,28 +1,47 @@
 <template>
-  <div class="login-page">
-    <div class="login-container">
+  <div class="login__page">
+    <section class="section-login">
+      <div class="row">
+        <div class="col-1-of-2">
+          <h2 class="heading--secondary">Don't have an account?</h2>
+          <span class="line line--blue"></span>
+          <p
+            class="paragraph--large"
+          >Lorem ipsum dolor sit, amet consectetur adipisicing elit. Illo, et, labore omnis consectetur esse ut quam eligendi nesciunt distinctio sequi recusandae cum nihil a tenetur!</p>
+          <button class="btn btn--primary" @click="toSignUp">Sign Up</button>
+        </div>
+        <div class="col-1-of-2">
+          <h2 class="heading--secondary">Have an account?</h2>
+          <span class="line line--blue"></span>
+          <p class="paragraph--large">Lorem, ipsum dolor sit amet consectetur adipisicing elit.</p>
+          <button class="btn btn--primary" @click="toLogin">Login</button>
+          <app-form :login="login" />
+        </div>
+      </div>
+    </section>
+    <!-- <div class="login__container">
       <div class="row h-100">
         <div class="col-1-of-2 h-100">
-          <div class="login-content">
-            <h2 class="heading-secondary">Don't have an account?</h2>
+          <div class="login__content">
+            <h2 class="heading__secondary">Don't have an account?</h2>
             <span class="blue-line"></span>
             <p
-              class="login-text"
+              class="login__text"
             >Lorem ipsum dolor sit, amet consectetur adipisicing elit. Illo, et, labore omnis consectetur esse ut quam eligendi nesciunt distinctio sequi recusandae cum nihil a tenetur!</p>
-            <button class="btn btn-primary" @click="toSignUp">Sign Up</button>
+            <button class="btn btn__primary" @click="toSignUp">Sign Up</button>
           </div>
         </div>
         <div class="col-1-of-2 h-100">
-          <div class="login-content">
-            <h2 class="heading-secondary">Have an account?</h2>
-            <span class="blue-line"></span>
-            <p class="login-text">Lorem, ipsum dolor sit amet consectetur adipisicing elit.</p>
-            <button class="btn btn-primary" @click="toLogin">Login</button>
+          <div class="login__content">
+            <h2 class="heading__secondary">Have an account?</h2>
+            <span class="blue__line"></span>
+            <p class="login__text">Lorem, ipsum dolor sit amet consectetur adipisicing elit.</p>
+            <button class="btn btn__primary" @click="toLogin">Login</button>
           </div>
         </div>
       </div>
       <app-form :login="login" />
-    </div>
+    </div>-->
   </div>
 </template>
 
